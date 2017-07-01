ba008e8de36f4e8f9b018e488cc1e3b18c7a3cda
SparxDev <sparxdev@outlook.de>
2017-07-01 22:56:47 +0200

    Updated files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -19,7 +19,7 @@ $(function() {
   ws.onmessage = function(message) {
     message = JSON.parse(message.data);
     console.log(message);
-    var tweet = '<div class="tweet"><img src="' + message["profile_img"] + '" class="tweet-avatar"><div class="tweet-political-real green">Grüne</div><div class="tweet-political-bot blue">AfD</div><div class="tweet-content"><h1>' + message["name"] + '</h1><h2>@' + message["handle"] + '</h2><p>' + message["text"] + '</p><p class="timestamp">' + formatDate(new Date(message["date"])) + '</p></div><div class="progress"><div class="bar" style="width:75%;"></div></div></div>';
+    var tweet = '<div class="tweet"><img src="' + message["profile_img"] + '" class="tweet-avatar"><div class="tweet-political-real ' + message["real_party"] + '">' + message["real_party"] + '</div><div class="tweet-political-bot ' + message["guessed_party"] + '">' + message["guessed_party"] + '</div><div class="tweet-content"><h1>' + message["name"] + '</h1><h2>@' + message["handle"] + '</h2><p>' + message["text"] + '</p><p class="timestamp">' + formatDate(new Date(message["date"])) + '</p></div><div class="progress"><div class="bar" style="width:75%;"></div></div></div>';
     $(tweet).prependTo($(".timeline")).hide().show("drop", 500);
   };
 });
